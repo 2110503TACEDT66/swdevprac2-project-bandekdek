80f741ac313420c7b1b006f9e65676404888d62c
konglol524 <kopmul26@gmail.com>
2024-03-20 15:10:54 +0700

    fix layout path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import TopMenu from '@/components/TopMenu'
+import TopMenu from '../components/TopMenu'
 
 export const metadata: Metadata = {
   title: 'Car Rental by Bandekdek',
