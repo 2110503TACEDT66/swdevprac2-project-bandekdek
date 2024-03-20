85a40451912d2e6ca11d8bb8daab5e0a8f5e021a
konglol524 <kopmul26@gmail.com>
2024-03-20 15:43:43 +0700

    added button in Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,11 @@
 
 import Image from "next/image"
+import { Button } from '@mui/material';
+import Link from 'next/link';
+
 
 export default function Banner () {
+
 
     return (
         <div className="p-5 m-0 w-[100vw] h-[100vh] block relative">
@@ -10,6 +14,14 @@ export default function Banner () {
                 <h1 className="text-5xl font-semibold text-white bg-black/[.50] p-3 w-fit mx-auto rounded-md mb-3">Vaccine Service Center</h1>
                 <h3 className="text-2xl font-serif text-white  bg-black/[.50] p-2 w-fit mx-auto rounded-md">Come get your booster today</h3>
             </div>
+
+            <Link href='/reservation' className='absolute bottom-0 right-0 m-10'>
+                <Button variant="outlined" size="large" color="error"  className="p-5 font-bold" >
+                    Make Your Reservation
+                </Button>            
+            </Link>
+
+
         </div>
     )
 }
