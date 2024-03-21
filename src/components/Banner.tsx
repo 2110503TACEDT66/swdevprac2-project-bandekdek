8f77656b689cd93aa121551407254c7401daf4aa
konglol524 <kopmul26@gmail.com>
2024-03-21 12:07:43 +0700

    changed banner text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,11 +11,11 @@ export default function Banner () {
         <div className="p-5 m-0 w-[100vw] h-[100vh] block relative">
             <Image src='/img/banner.jpg' alt='cover' fill={true} objectFit='cover'/>
             <div className="relative top-[100px] z-20 text-center">
-                <h1 className="text-5xl font-semibold text-white bg-black/[.50] p-3 w-fit mx-auto rounded-md mb-3">Vaccine Service Center</h1>
-                <h3 className="text-2xl font-serif text-white  bg-black/[.50] p-2 w-fit mx-auto rounded-md">Come get your booster today</h3>
+                <h1 className="text-5xl font-semibold text-white bg-black/[.50] p-3 w-fit mx-auto rounded-md mb-3">BanDekDek Car Rental</h1>
+                <h3 className="text-2xl font-serif text-white  bg-black/[.50] p-2 w-fit mx-auto rounded-md">Come rent your car today</h3>
             </div>
 
-            <Link href='/reservation' className='absolute bottom-0 sm:right-0 m-10  left-[50%]' >
+            <Link href='/reservation' className='w-fit absolute left-0 right-0 bottom-0 mb-10 mx-auto md:ml-auto md:mr-10' >
                 <Button variant="outlined" size="large" color="error"  className="p-5 font-bold" >
                     Make Your Reservation
                 </Button>            
