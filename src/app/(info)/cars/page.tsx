65d2b1990b323b2fccf2344a02378e1665157d27
konglol524 <kopmul26@gmail.com>
2024-03-22 11:36:53 +0700

    added next config url

--- src/app/(info)/cars/page.tsx
+++ src/app/(info)/cars/page.tsx
@@ -1,15 +1,11 @@
 import CarCatalog from "@/components/CarCatalog"
-import { Suspense } from "react"
-import { LinearProgress } from "@mui/material"
 
 
 export default function cars() {
     return (
       <main className="text-center p-5 mt-[60px] h-[100vh] bg-black bg-opacity-15">
         <h1 className="text-4xl font-medium text-white">Our Top Cars</h1>
-        <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
              <CarCatalog/>     
-        </Suspense>
         <hr className="mt-auto"/>
       </main>
     )
