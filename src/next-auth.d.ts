d54409bd6456019f18960dccf8b1b1da4793bc22
konglol524 <kopmul26@gmail.com>
2024-03-24 16:54:33 +0700

    improved Session interface

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -3,7 +3,12 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      data: any;
+      data: {
+          _id: string;
+          name: string;
+          email: string;
+          role: string;
+      };
       token: string;
     };
   }
