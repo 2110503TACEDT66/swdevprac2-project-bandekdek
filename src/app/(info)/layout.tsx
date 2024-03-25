6a5682155ac4d3dd9594cf6db54137ce06f9076d
konglol524 <kopmul26@gmail.com>
2024-03-25 20:49:03 +0700

    changed background color of cars page

--- src/app/(info)/layout.tsx
+++ src/app/(info)/layout.tsx
@@ -2,7 +2,7 @@
 
 export default function InfoLayout({ children,}: {children: React.ReactNode}) {
     return (
-        <div className="h-[200vh] bg-gradient-to-tl from-red-700 from-20% via-amber-600 to-purple-700 to-90%">
+        <div className="h-[200vh] bg-gradient-to-tl from-red-600 to-slate-800">
             {children}
         </div>
     )
--- src/components/CarCatalog.tsx
+++ src/components/CarCatalog.tsx
@@ -9,7 +9,7 @@ export default async function CarCatalog() {
     return(
          <div className='p-10'>
             <h1 className="text-5xl font-bold text-white my-[30px]">Our Top Cars</h1>
-            <div className='flex flex-row content-around justify-around flex-wrap bg-gradient-to-tl shadow-lg rounded-lg from-red-700 from-20% via-amber-600 to-purple-700 to-90%'>
+            <div className='flex flex-row content-around justify-around flex-wrap bg-gradient-to-tl shadow-lg rounded-lg  from-red-700 from-50% to-slate-800 '>
                 {
                     cars.map((carItem:Car)=>(
                             <Card carItem={carItem}/>
