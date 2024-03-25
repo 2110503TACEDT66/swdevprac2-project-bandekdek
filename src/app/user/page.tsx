c0014de2f9b82e034ef59395804cb521e1c96117
Phuree <phureeppp@gmail.com>
2024-03-25 19:20:08 +0700

    style /user

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -19,8 +19,8 @@ export default async function user() {
   return (
     <>
       {session && (
-        <main className="p-5 m-5 bg-slate-100 mt-[60px]">
-          <div className="text-2xl">{profile.data.name}</div>
+        <main className="mt-[70px] flex justify-center">
+          {/* <div className="text-2xl">{profile.data.name}</div>
           <table className="border-separate table-auto border-spacing-2">
             <tbody>
               <tr>
@@ -36,7 +36,31 @@ export default async function user() {
                 <td>{createdAt?.toString()}</td>
               </tr>
             </tbody>
-          </table>
+          </table> */}
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Your Profile!</h2>
+              <table className="border-separate table-auto border-spacing-2">
+                <tbody>
+                  <tr>
+                    <td>Email :</td>
+                    <td>{profile.data.email}</td>
+                  </tr>
+                  <tr>
+                    <td>Tel. :</td>
+                    <td>{profile.data.telephone}</td>
+                  </tr>
+                  <tr>
+                    <td>Member Since :</td>
+                    <td>{createdAt?.toString()}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">{profile.data.role}</button>
+              </div>
+            </div>
+          </div>
         </main>
       )}
 
--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import deleteBooking from "@/libs/deleteBooking";
 import updateBooking from "@/libs/updateBooking";
 import { useRouter } from "next/navigation";
-
+import { FaInfoCircle } from "react-icons/fa";
 export default function BookingList({
   bookings,
   token,
@@ -53,153 +53,163 @@ export default function BookingList({
   return (
     <>
       {bookings && (
-        <main className="p-5 m-5 bg-slate-100 mt-[60px]">
-          <header>
-            <h1>count : {bookings.count}</h1>
-            <h1>totalCost :{bookings.totalCost}</h1>
-          </header>
-          <div className="w-full mt-4">
-            <table className="table table-zebra">
-              <thead>
-                <tr>
-                  <th className="text-sm">No.</th>
-                  <th className="text-sm">Car</th>
-                  <th className="text-sm">bookingDate</th>
-                  <th className="text-sm">daySpend</th>
-                  <th className="text-sm">Provider</th>
-                  <th className="text-sm">Cost</th>
-                </tr>
-              </thead>
-              <tbody>
-                {bookings.data.map((item: Booking, idx) => (
-                  <tr className="font-[500]" key={idx}>
-                    <th>{idx + 1}</th>
-                    {editID === item._id ? (
-                      <div className="flex flex-col">
-                        <label>Car</label>
-                        <input
-                          value={form.car !== undefined ? form.car : item.car}
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="car..."
-                          name="car"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>bookingDate</label>
-                        <input
-                          value={
-                            form.bookingDate !== undefined
-                              ? form.bookingDate
-                              : item.bookingDate
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="bookingDate..."
-                          name="bookingDate"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>daySpend</label>
-                        <input
-                          value={
-                            form.daySpend !== undefined
-                              ? form.daySpend
-                              : item.daySpend
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="number"
-                          placeholder="daySpend..."
-                          name="daySpend"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>createdAt</label>
-                        <input
-                          value={
-                            form.createdAt !== undefined
-                              ? form.createdAt
-                              : item.createdAt
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="createdAt..."
-                          name="createdAt"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>Provider</label>
-                        <input
-                          value={
-                            form.rentalProvider !== undefined
-                              ? form.rentalProvider.name
-                              : item.rentalProvider.name
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="Provider..."
-                          name="Provider"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>Cost</label>
-                        <input
-                          value={
-                            form.rentalProvider !== undefined
-                              ? form.rentalProvider.cost
-                              : item.rentalProvider.cost
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="number"
-                          placeholder="cost..."
-                          name="cost"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                      </div>
-                    ) : (
-                      <>
-                        <td>{item.car}</td>
-                        <td>{item.bookingDate}</td>
-                        <td>{item.daySpend}</td>
-                        <td>{item.rentalProvider.name}</td>
-                        <td>{item.rentalProvider.cost}</td>
-                      </>
-                    )}
-
-                    <td className="grid md:grid-cols-2 md:max-w-[200px]">
+        <div className="collapse bg-base-200 mt-8 ">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl flex justify-center">
+            <FaInfoCircle />
+          </div>
+          <div className="collapse-content">
+            <header>
+              <h1 className="text-xl font-[700]">
+                Booking Count : {bookings.count}
+              </h1>
+              <h1 className="text-xl font-[600]">
+                Total Cost : {bookings.totalCost.toLocaleString()}
+              </h1>
+            </header>{" "}
+            <div className="w-full mt-4">
+              <table className="table table-zebra">
+                <thead>
+                  <tr>
+                    <th className="text-sm">No.</th>
+                    <th className="text-sm">Car</th>
+                    <th className="text-sm">bookingDate</th>
+                    <th className="text-sm">daySpend</th>
+                    <th className="text-sm">Provider</th>
+                    <th className="text-sm">Cost</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {bookings.data.map((item: Booking, idx) => (
+                    <tr className="font-[500]" key={idx}>
+                      <th>{idx + 1}</th>
                       {editID === item._id ? (
-                        <>
-                          <button
-                            onClick={() => handleCancel()}
-                            className="md:mr-2 btn btn-error hover:opacity-75"
-                          >
-                            <TiCancel />
-                          </button>
-                          <button
-                            onClick={() => handleSave(item._id)}
-                            className="btn btn-success"
-                          >
-                            <FaRegSave />
-                          </button>
-                        </>
+                        <div className="flex flex-col">
+                          <label>Car</label>
+                          <input
+                            value={form.car !== undefined ? form.car : item.car}
+                            onChange={(e) => handleChange(e)}
+                            type="text"
+                            placeholder="car..."
+                            name="car"
+                            className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                          />
+                          <label>bookingDate</label>
+                          <input
+                            value={
+                              form.bookingDate !== undefined
+                                ? form.bookingDate
+                                : item.bookingDate
+                            }
+                            onChange={(e) => handleChange(e)}
+                            type="text"
+                            placeholder="bookingDate..."
+                            name="bookingDate"
+                            className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                          />
+                          <label>daySpend</label>
+                          <input
+                            value={
+                              form.daySpend !== undefined
+                                ? form.daySpend
+                                : item.daySpend
+                            }
+                            onChange={(e) => handleChange(e)}
+                            type="number"
+                            placeholder="daySpend..."
+                            name="daySpend"
+                            className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                          />
+                          <label>createdAt</label>
+                          <input
+                            value={
+                              form.createdAt !== undefined
+                                ? form.createdAt
+                                : item.createdAt
+                            }
+                            onChange={(e) => handleChange(e)}
+                            type="text"
+                            placeholder="createdAt..."
+                            name="createdAt"
+                            className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                          />
+                          <label>Provider</label>
+                          <input
+                            value={
+                              form.rentalProvider !== undefined
+                                ? form.rentalProvider.name
+                                : item.rentalProvider.name
+                            }
+                            onChange={(e) => handleChange(e)}
+                            type="text"
+                            placeholder="Provider..."
+                            name="Provider"
+                            className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                          />
+                          <label>Cost</label>
+                          <input
+                            value={
+                              form.rentalProvider !== undefined
+                                ? form.rentalProvider.cost
+                                : item.rentalProvider.cost
+                            }
+                            onChange={(e) => handleChange(e)}
+                            type="number"
+                            placeholder="cost..."
+                            name="cost"
+                            className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                          />
+                        </div>
                       ) : (
                         <>
-                          <button
-                            onClick={() => handleDel(item._id)}
-                            className="md:mr-2 btn btn-error hover:opacity-75"
-                          >
-                            <MdDeleteOutline />
-                          </button>
-                          <button
-                            onClick={() => setEditID(item._id)}
-                            className="btn btn-info"
-                          >
-                            <FaRegEdit />
-                          </button>
+                          <td>{item.car}</td>
+                          <td>{item.bookingDate}</td>
+                          <td>{item.daySpend}</td>
+                          <td>{item.rentalProvider.name}</td>
+                          <td>{item.rentalProvider.cost}</td>
                         </>
                       )}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+
+                      <td className="grid md:grid-cols-2 md:max-w-[200px]">
+                        {editID === item._id ? (
+                          <>
+                            <button
+                              onClick={() => handleCancel()}
+                              className="md:mr-2 btn btn-error hover:opacity-75"
+                            >
+                              <TiCancel />
+                            </button>
+                            <button
+                              onClick={() => handleSave(item._id)}
+                              className="btn btn-success"
+                            >
+                              <FaRegSave />
+                            </button>
+                          </>
+                        ) : (
+                          <>
+                            <button
+                              onClick={() => handleDel(item._id)}
+                              className="md:mr-2 btn btn-error hover:opacity-75"
+                            >
+                              <MdDeleteOutline />
+                            </button>
+                            <button
+                              onClick={() => setEditID(item._id)}
+                              className="btn btn-info"
+                            >
+                              <FaRegEdit />
+                            </button>
+                          </>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </main>
+        </div>
       )}
     </>
   );
