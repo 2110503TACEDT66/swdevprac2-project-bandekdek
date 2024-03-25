28bec47f3ea2ac40bcc861e91bcc423a4793c888
Phuree <phureeppp@gmail.com>
2024-03-25 17:50:47 +0700

    register styling

--- src/app/(info)/shops/page.tsx
+++ src/app/(info)/shops/page.tsx
@@ -1,11 +1,9 @@
 export default function shops() {
-    return (
+  return (
+    <main className="text-center p-5 mt-[60px]">
+      <h1 className="text-5xl font-bold text-white mt-[60px]">Our Top Shops</h1>
 
-      <main className="text-center p-5 mt-[60px]">
-        <h1 className="text-5xl font-bold text-white">Our Top Shops</h1>
-             
-        <hr className="mt-auto"/>
-      </main>
-    )
+      <hr className="mt-auto" />
+    </main>
+  );
 }
-
