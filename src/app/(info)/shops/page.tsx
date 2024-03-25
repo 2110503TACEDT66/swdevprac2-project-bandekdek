99a49c322df5fe0395d841334c37912e6693a921
konglol524 <kopmul26@gmail.com>
2024-03-25 16:51:57 +0700

    small fix to shop pages

--- src/app/(info)/shops/page.tsx
+++ src/app/(info)/shops/page.tsx
@@ -1,10 +1,7 @@
 export default function shops() {
     return (
 
-      <main className="text-center p-5 mt-[60px]">
-        <h1 className="text-5xl font-bold text-white">Our Top Shops</h1>
-             
-        <hr className="mt-auto"/>
+      <main className="text-center">
       </main>
     )
 }
