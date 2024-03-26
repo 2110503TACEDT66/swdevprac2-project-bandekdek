07f894b2893ab6ce1c782e48841981974a173856
Thanaphat Chaiboonrueang <tan.thanaphat4@gmail.com>
2024-03-26 11:08:39 +0700

    Remake Reservation

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -5,16 +5,17 @@ import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 
 export default function Reservation() {
-  const { data:session } = useSession()
-  
   const urlParams = useSearchParams();
   const shopID = urlParams.get('id')
   console.log(shopID)
 
   return (
-    <main className="p-1 h-[200vh] bg-gradient-to-tl from-red-600 to-yellow-400">
-        <div className="mt-[85px] text-4xl text-white font-bold">Your Reservations ({session?.user.data.name})</div>
-        <div className="bg-white h-[5px] mt-[20px] mb-[20px] w-[80%] sm:w-[60%] md:w-[50%] m-auto rounded-xl"></div>
+    <main className="p-1 h-[200vh] bg-gradient-to-tl from-red-600 to-slate-950">
+        <div className="mt-[85px] text-4xl text-white font-bold">Make Reservation</div>
+        <div className="bg-white h-[5px] mt-[20px] mb-[30px] w-[80%] sm:w-[60%] md:w-[50%] m-auto rounded-xl"></div>
+        <div className="bg-white h-[40%] w-[40%] m-auto">
+
+        </div>
     </main>
   )
 }
